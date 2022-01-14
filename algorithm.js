e22ee371d6f426f100dc1c9c27fc305d24c8299b
Jason <45002790+w2015long@users.noreply.github.com>
2022-01-14 14:14:38 +0800

    Update algorithm.js
    栈查找

--- algorithm.js
+++ algorithm.js
@@ -175,6 +175,28 @@ export default {
         }
     },
     /**
+     * 深度遍历查找
+     * @param {*} tree 树形数据
+     * @param {*} target 想要查找的目标
+     */
+    DFS (tree, target) {
+      // 模拟栈，管理结点
+      let stack = [...tree];
+      while (stack.length) {
+        // 栈顶节点出栈
+        let node = stack.pop()
+        // 查找到目标，退出
+        if (node.value === target) {
+          return target;
+        }
+        if (node.children && node.children.length) {
+          // 将候选顶点入栈，进行下一次循环
+          stack.push(...node.children.reverse())
+        }
+      }
+    },
+
+    /**
      * 寻找出无重复字符的最长子串 
      */
     lengthOfLongestSubstring (s) {
